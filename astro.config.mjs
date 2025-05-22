9f3f01a4ab4e5d4d04209fff6b1975172730ed20
eppleee <kgiles3@students.towson.edu>
2025-05-22 14:58:02 -0400

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
+  output: "server",
+  adapter: vercel(),
+  security: {
+    checkOrigin: true,
+  },
+});
