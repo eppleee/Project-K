2882ca1757b3cf77834a2647667b5d0126f8b3fb
eppleee <kgiles3@students.towson.edu>
2025-06-10 19:48:56 -0400

    Made a Home file and imported button and tabswitch from components

---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button.tsx";
+import TabSwitch from "@/components/TabSwitch.tsx";
+
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <p className="text-7xl text-[#f474b6]">♥</p>
+      <p className="italic text-3xl px-7 md:px-0">Kourtney</p>
+    </div>
+  );
+};
+
+export default Home;
